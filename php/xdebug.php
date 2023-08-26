e6482907aef3a98d45bc8dd65a5348c54349b48f
Patrik Casadei DellaChiesa <p.casadei@teamsystem.com>
2023-08-26 06:44:43 +0200

    php - sample use of xdebug

---
+++ php/xdebug.php
@@ -0,0 +1,6 @@
+<?php
+    $arr = [12, 3, "test", "lastname" => "arias"];
+    // var_dump instruction show information about var
+    // using xdebug information are better visible
+    var_dump($arr);
+?>
